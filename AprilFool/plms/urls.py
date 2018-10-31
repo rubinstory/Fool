95bd3ab486a27a9dbc9577d4f2024a6afca0aa64
JUYOUNG <rubinstory@naver.com>
2018-10-31 01:44:22 +0000

    t

--- AprilFool/plms/urls.py
+++ AprilFool/plms/urls.py
@@ -2,6 +2,11 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
+        url(r'^Course.html/$', views.Course, name = 'Course'),
+        url(r'^Course.html/CProgramming.html/$', views.CProgramming, name = 'CProgramming'),
+        url(r'^Course.html/CProgramming.html/Notice.html/$', views.Notice, name = 'Notice'),
+        url(r'^Course.html/CProgramming.html/Notice.html/Retest.html/$', views.Retest, name = 'Retest'),
+        url(r'^Course.html/CProgramming.html/Notice.html/TestList.html/$', views.TestList, name = 'TestList'),
         url(r'^$', views.plms, name = 'plms'),
-        url(r'^$', views.Course, name = 'Course'),
+        url(r'^p1ms.pusan.ac.kr/$', views.plms, name = 'plms'),
 ]
--- AprilFool/plms/views.py
+++ AprilFool/plms/views.py
@@ -2,6 +2,13 @@ from django.shortcuts import render
 
 def plms(request):
     return render(request, 'PLMS/plms.html', {})
-
 def Course(request):
-    return redner(request, 'PLMS/Course.html', {})
+    return render(request, 'PLMS/Course.html', {})
+def CProgramming(request):
+    return render(request, 'PLMS/CProgramming.html', {})
+def Notice(request):
+    return render(request, 'PLMS/Notice.html', {})
+def Retest(request):
+    return render(request, 'PLMS/Retest.html', {})
+def TestList(request):
+    return render(request, 'PLMS/TestList.html', {})
